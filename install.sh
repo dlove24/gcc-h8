f30241e26aa0dc52c4f39b0c98142c2eafafc897
David Love <david@homeunix.org.uk>
2012-11-18 16:07:09 +0000

    Build the NewLib C library

--- install.sh
+++ install.sh
@@ -35,9 +35,21 @@ cd $BASE_DIR
 mkdir -p build/gcc
 cd build/gcc
 
-$BASE_DIR/gcc/configure --target="h8300-hms" --prefix=$PREFIX --enable-languages="c" --with-newlib --with-headers=$BASE_DIR/newlib/newlib/libc/include
-make CFLAGS=$CFLAGS LANGUAGES="c"
+$BASE_DIR/gcc/configure --target="h8300-hms" --prefix=$PREFIX --enable-languages="c,c++" --with-newlib --with-headers=$BASE_DIR/newlib/newlib/libc/include make CFLAGS=$CFLAGS LANGUAGES="c,c++"
 
 # Ask to install the compiler
 sudo make install
 
+##
+## Build the newlib C library
+##
+
+cd $BASE_DIR
+mkdir -p build/gcc
+cd build/gcc
+
+$BASE_DIR/newlib/configure --target=h8300-hms --prefix=$PREFIX
+make FLAGS=$CFLAGS all
+
+# Ask to install the library
+sudo make install
